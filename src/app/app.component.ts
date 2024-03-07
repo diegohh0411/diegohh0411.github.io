dacda3c457f529da68aa225d439d0299e4604ac5
diego hernández herrera <diegohh0411@gmail.com>
2024-03-07 13:24:32 -0600

    v2.0.5 deploy, added custom ICO and fixed error that didn't let it deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { NavigationComponent } from "./navigation/navigation.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'partage';
+  title = "Diego's Journal";
 }
